14e3e798586b5ceb76df05040617e29ff686f239
Adam Goldfine <tulaneadam21@gmail.com>
2023-06-10 19:12:55 -0400

    .

---
+++ projects/startsys-angular-speech-recognition/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of startsys-angular-speech-recognition
+ */
+
+export * from './lib/startsys-angular-speech-recognition.service';
+export * from './lib/startsys-angular-speech-recognition.component';
+export * from './lib/startsys-angular-speech-recognition.module';
