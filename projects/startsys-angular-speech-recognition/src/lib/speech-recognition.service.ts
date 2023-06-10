bae98b5cfe419025f13fe309c9f36ae09d43deac
Adam Goldfine <tulaneadam21@gmail.com>
2023-06-10 19:27:19 -0400

    .

---
+++ projects/startsys-angular-speech-recognition/src/lib/speech-recognition.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpeechRecognitionService {
+  // Add your speech recognition code here
+}
--- projects/startsys-angular-speech-recognition/src/public-api.ts
+++ projects/startsys-angular-speech-recognition/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of startsys-angular-speech-recognition
  */
 
-export * from './lib/startsys-angular-speech-recognition.service';
-export * from './lib/startsys-angular-speech-recognition.component';
-export * from './lib/startsys-angular-speech-recognition.module';
+export * from './lib/speech-recognition.service';
+
